9ccedd267f12137271f96515574b3b9abb4f8c66
Everything Windows <windowseverything61@gmail.com>
2023-09-20 18:07:14 +0700

    initial choosing function

--- WindowsISODownloader/Form1.Designer.vb
+++ WindowsISODownloader/Form1.Designer.vb
@@ -36,7 +36,20 @@ Partial Class Form1
         Me.Label6 = New System.Windows.Forms.Label()
         Me.ComboBox4 = New System.Windows.Forms.ComboBox()
         Me.Label7 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -44,7 +57,7 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.0!)
         Me.Label1.ForeColor = System.Drawing.Color.MidnightBlue
-        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Location = New System.Drawing.Point(13, 30)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(322, 25)
         Me.Label1.TabIndex = 0
@@ -54,7 +67,7 @@ Partial Class Form1
         '
         Me.Label2.Font = New System.Drawing.Font("Segoe UI", 10.0!)
         Me.Label2.ForeColor = System.Drawing.Color.Black
-        Me.Label2.Location = New System.Drawing.Point(13, 40)
+        Me.Label2.Location = New System.Drawing.Point(13, 57)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(352, 45)
         Me.Label2.TabIndex = 0
@@ -65,7 +78,7 @@ Partial Class Form1
         '
         Me.ComboBox1.FormattingEnabled = True
         Me.ComboBox1.Items.AddRange(New Object() {"Client", "Client Beta", "Server", "Server Beta"})
-        Me.ComboBox1.Location = New System.Drawing.Point(17, 126)
+        Me.ComboBox1.Location = New System.Drawing.Point(75, 143)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(151, 21)
         Me.ComboBox1.TabIndex = 1
@@ -73,7 +86,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(17, 105)
+        Me.Label3.Location = New System.Drawing.Point(72, 122)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(42, 13)
         Me.Label3.TabIndex = 2
@@ -83,7 +96,7 @@ Partial Class Form1
         '
         Me.ComboBox2.FormattingEnabled = True
         Me.ComboBox2.Items.AddRange(New Object() {"Windows 1.x", "Windows 2.x", "Windows 3.x", "Windows 95", "Windows 98", "Windows ME", "Windows XP", "Windows Vista", "Windows 7", "Windows 8", "Windows 8.1", "Windows 10", "Windows 11"})
-        Me.ComboBox2.Location = New System.Drawing.Point(17, 181)
+        Me.ComboBox2.Location = New System.Drawing.Point(75, 209)
         Me.ComboBox2.Name = "ComboBox2"
         Me.ComboBox2.Size = New System.Drawing.Size(151, 21)
         Me.ComboBox2.TabIndex = 1
@@ -91,7 +104,7 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(17, 160)
+        Me.Label4.Location = New System.Drawing.Point(72, 188)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(48, 13)
         Me.Label4.TabIndex = 2
@@ -99,7 +112,8 @@ Partial Class Form1
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(388, 13)
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.Location = New System.Drawing.Point(388, 30)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(400, 225)
         Me.PictureBox1.TabIndex = 3
@@ -107,18 +121,18 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(17, 407)
+        Me.Button1.Location = New System.Drawing.Point(17, 381)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(148, 31)
+        Me.Button1.Size = New System.Drawing.Size(209, 31)
         Me.Button1.TabIndex = 4
         Me.Button1.Text = "Download!"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Label5
         '
-        Me.Label5.Location = New System.Drawing.Point(388, 245)
+        Me.Label5.Location = New System.Drawing.Point(388, 262)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(400, 193)
+        Me.Label5.Size = New System.Drawing.Size(400, 153)
         Me.Label5.TabIndex = 5
         Me.Label5.Text = resources.GetString("Label5.Text")
         '
@@ -126,7 +140,7 @@ Partial Class Form1
         '
         Me.ComboBox3.FormattingEnabled = True
         Me.ComboBox3.Items.AddRange(New Object() {"Windows 1.x", "Windows 2.x", "Windows 3.x", "Windows 95", "Windows 98", "Windows ME", "Windows XP", "Windows Vista", "Windows 7", "Windows 8", "Windows 8.1", "Windows 10", "Windows 11"})
-        Me.ComboBox3.Location = New System.Drawing.Point(18, 239)
+        Me.ComboBox3.Location = New System.Drawing.Point(75, 270)
         Me.ComboBox3.Name = "ComboBox3"
         Me.ComboBox3.Size = New System.Drawing.Size(151, 21)
         Me.ComboBox3.TabIndex = 1
@@ -134,7 +148,7 @@ Partial Class Form1
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(18, 218)
+        Me.Label6.Location = New System.Drawing.Point(72, 249)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(69, 13)
         Me.Label6.TabIndex = 2
@@ -144,7 +158,7 @@ Partial Class Form1
         '
         Me.ComboBox4.FormattingEnabled = True
         Me.ComboBox4.Items.AddRange(New Object() {"Windows 1.x", "Windows 2.x", "Windows 3.x", "Windows 95", "Windows 98", "Windows ME", "Windows XP", "Windows Vista", "Windows 7", "Windows 8", "Windows 8.1", "Windows 10", "Windows 11"})
-        Me.ComboBox4.Location = New System.Drawing.Point(18, 299)
+        Me.ComboBox4.Location = New System.Drawing.Point(75, 334)
         Me.ComboBox4.Name = "ComboBox4"
         Me.ComboBox4.Size = New System.Drawing.Size(151, 21)
         Me.ComboBox4.TabIndex = 1
@@ -152,17 +166,86 @@ Partial Class Form1
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(18, 278)
+        Me.Label7.Location = New System.Drawing.Point(72, 313)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(73, 13)
+        Me.Label7.Size = New System.Drawing.Size(135, 13)
         Me.Label7.TabIndex = 2
-        Me.Label7.Text = "Edition/Build :"
+        Me.Label7.Text = "Edition/Build/Architecture :"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.White
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(24, 20)
+        Me.ToolStripMenuItem1.Text = "?"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Black
+        Me.PictureBox2.Location = New System.Drawing.Point(17, 118)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(48, 48)
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Black
+        Me.PictureBox3.Location = New System.Drawing.Point(17, 182)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(48, 48)
+        Me.PictureBox3.TabIndex = 7
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Black
+        Me.PictureBox4.Location = New System.Drawing.Point(17, 245)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(48, 48)
+        Me.PictureBox4.TabIndex = 7
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.Black
+        Me.PictureBox5.Location = New System.Drawing.Point(17, 309)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(48, 48)
+        Me.PictureBox5.TabIndex = 7
+        Me.PictureBox5.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 424)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.PictureBox1)
@@ -176,9 +259,17 @@ Partial Class Form1
         Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Windows ISO Downloader"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -197,4 +288,12 @@ Partial Class Form1
     Friend WithEvents Label6 As Label
     Friend WithEvents ComboBox4 As ComboBox
     Friend WithEvents Label7 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
 End Class
--- WindowsISODownloader/Form1.vb
+++ WindowsISODownloader/Form1.vb
@@ -1,3 +1,50 @@
 ﻿Public Class Form1
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ComboBox2.SelectedItem = Nothing
+        ComboBox3.SelectedItem = Nothing
+        ComboBox4.SelectedItem = Nothing
+        Select Case ComboBox1.SelectedItem
+            Case "Client"
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Windows 10")
+            Case "Client Beta"
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Windows 11")
+        End Select
+    End Sub
 
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        ComboBox3.SelectedItem = Nothing
+        ComboBox4.SelectedItem = Nothing
+        Select Case ComboBox1.SelectedItem
+            Case "Client"
+                Select Case ComboBox2.SelectedItem
+                    Case "Windows 10"
+                        ComboBox3.Items.Clear()
+                        ComboBox3.Items.Add("22H2")
+                End Select
+            Case "Client Beta"
+                Select Case ComboBox2.SelectedItem
+                    Case "Nickel"
+                        ComboBox3.Items.Clear()
+                        ComboBox3.Items.Add("21996.1")
+                        ComboBox3.Items.Add("22000.9")
+                End Select
+        End Select
+    End Sub
+
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+        ComboBox4.SelectedItem = Nothing
+        Select Case ComboBox1.SelectedItem
+            Case "Client"
+                Select Case ComboBox2.SelectedItem
+                    Case "Windows 10"
+                        Select Case ComboBox3.SelectedItem
+                            Case "22H2"
+                                ComboBox4.Items.Clear()
+                                ComboBox4.Items.Add("x64")
+                        End Select
+                End Select
+        End Select
+    End Sub
 End Class
